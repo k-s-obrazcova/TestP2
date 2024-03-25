f32853eb1347f8fd4b21a37fdc99d75734b2335b
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-03-25 12:50:53 +0300

    News

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ('Hello, World! Test text,'
-            ' para P50-5-20, 25.05.2024. Test Check Style Project')
+    return 'Hello, World! Test text'
 
 
 app.run()
