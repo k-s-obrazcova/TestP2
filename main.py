ca07aa33c88daab7af91d91f96e42803b4fc36e5
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-03-26 11:05:22 +0300

    New P4

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ('Hello, World! Tesdfghjkfghjghghghghghghghghgt'
-            ' fdfdfdfdfdfdfdfdfdfdfdfdfdtext')
+    return 'Hello, World! Tesdfghjkfghjghghghghghghghghgt fdfdfdfdfdfdfdfdfdfdfdfdfdtext'
 
 
 app.run()
