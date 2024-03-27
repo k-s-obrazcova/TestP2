d3625605181e944281890e2e01d584ea463a180c
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-03-27 10:51:19 +0300

    Test

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ('Hello, World! Tesdfghjkfghjghghghghghghghgh'
-            ' gtfdfdfdfdfdfdfdfdfdfdfdfdfdtext')
+    return 'Hello, World! Tesdfghjkfghjghghghghghghghgh gtfdfdfdfdfdfdfdfdfdfdfdfdfdtext'
 
 
 app.run()
